4f8812218b1e68cbe650669f1cb0c9c0892a7cb7
ferdianzh <ferdianzh30@gmail.com>
2021-11-11 08:56:02 +0700

    First commit

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -1,14 +1,16 @@
 <?php $this->view("templates/header") ?>
 
-<main class="row">
-    <div class="col-4 bg-primary">
-        <p>menu</p>
-    </div>
-    <div class="col-8 bg-success">
-        <p>map</p>
-    </div>
-    <div>
-        <p><b>Test DB:</b> <?= $pangkalan['nama'] ?></p>
+<main class="container-fluid">
+    <div class="row">
+        <div class="col-4 bg-primary">
+            <p>menu</p>
+        </div>
+        <div class="col-8 bg-success">
+            <p>map</p>
+        </div>
+        <div>
+            <p><b>Test DB:</b> <?= $pangkalan['nama'] ?></p>
+        </div>
     </div>
 </main>
 
